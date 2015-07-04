5cc5124c91321ceac930ce200e70c84e513f4e77
Jonathan Wilde <hello@jwilde.me>
2015-07-04 15:52:54 -0700

    Adding configuration for webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var SRC = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: './main.js',
-  devtool: 'cheap-module-source-map',
-  output: {
-    path: './.build',
-    publicPath: '/.build/',
-    filename: 'bundle.js'
+  entry: [
+    'webpack-dev-server/client?http://localhost:6060',
+    'webpack/hot/only-dev-server',
+    './main.js'
+  ],
+  resolve: {
+    root: SRC,
+    alias: {
+      'uuid': 'node-uuid',
+      'typed-immutable': 'typed-immutable/lib/'
+    }
   },
   module: {
     loaders: [
+      {test : /\.js$/, loader: 'react-hot', include: SRC},
+
       // By default, node supports loading JSON via require(). Webpack does not,
       // so we have to shim that functionality with json-loader.
       {test: /\.json$/, loader: 'json-loader'},
@@ -21,11 +29,14 @@ module.exports = {
       {test: require.resolve('react'), loader: 'expose?React'}
     ]
   },
-  resolve: {
-    root: SRC,
-    alias: {
-      'uuid': 'node-uuid',
-      'typed-immutable': 'typed-immutable/lib/'
-    }
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  devtool: 'cheap-source-map',
+  output: {
+    path: path.join(__dirname, '.build'),
+    publicPath: '/.build/',
+    filename: 'bundle.js'
   }
 };
